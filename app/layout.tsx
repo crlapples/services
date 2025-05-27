a4d8c66d1d455f855ae817978f87dc07c28237ce
crlapples19 <crlapples19@gmail.com>
2025-05-27 19:23:49 +0000

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,23 +23,12 @@ export default function RootLayout(layoutProps: any) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="https://www.wix.com/favicon.ico" />
       </head>
-      {wixSession.wixClient ? (
-        <body className="parallax-background">
-          <Header />
-          <main className="bg-transparent min-h-[600px]">{children}</main>
-          <Footer />
-        </body>
-      ) : (
-        <body className="">
-          <main className="max-w-full-content mx-auto bg-gray-c2 pt-32">
-            <h1>
-              Page not available. Please add an environment variable called
-              NEXT_PUBLIC_WIX_CLIENT_ID, containing the client ID, to your
-              deployment provider.
-            </h1>
-          </main>
-        </body>
-      )}
+      
+      <body className="parallax-background">
+        <Header />
+        <main className="bg-transparent min-h-[600px]">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
